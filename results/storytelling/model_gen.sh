9b863480624263aeab418331bfbb80cbe46de06a
Smartmedia-AI <161900368+Smartmedia-AI@users.noreply.github.com>
2024-03-07 14:33:20 +0100

    Add files via upload

---
+++ results/storytelling/model_gen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source /cluster/home/penl/workspace/instructionGPT/bin/activate
+module purge
+module load Python/3.10.8-GCCcore-12.2.0
+module load CUDA/11.3.1
+
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python evaluation_gptj_6B.py 
+
+
